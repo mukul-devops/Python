0a1554fccc8e97ac6dd3d7e9a054cf9bff9ee208
Mukul Chahar <mukulchahar98135@gmail.com>
2026-07-15 11:38:08 +0530

    Update 01venv.py

--- Advanced Python 2/01venv.py
+++ Advanced Python 2/01venv.py
@@ -1,1 +1,13 @@
+#Virtual Environment - (venv)
+#It is an isolated space where you can install packages for a specific project without affecting other project or the python system
+'''
+Installation - write in terminal
+1 - pip install virtualenv             #Install the package of venv
+2 - virtualenv myprojectenv            # Rename of venv  as myprojectenv
+3 - .\myprojectenv\Scripts\activate    #Activation of venv
 
+for deactivation of venv - deactivate
+' pip freeze ' returns all the package installes in a given python envirnment along with the versions.
+the above command creats a file named ' requirements.txt ' in the same directory containing the output of ' pip freeze'
+
+'''
